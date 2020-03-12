7196cc044d586b8d98ca996611c61fe2d98bf849
Simon Herteby <s.herteby@gmail.com>
2020-03-12 21:50:24 +0100

    migrations

---
+++ src/Evergreen/Migrate/V9.elm
@@ -0,0 +1,44 @@
+module Evergreen.Migrate.V9 exposing (..)
+
+import Dict
+import Evergreen.Type.V7 as Old
+import Evergreen.Type.V9 as New
+import Lamdera.Migrations exposing (..)
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelMigrated
+        ( { page = New.StartPage }
+        , Cmd.none
+        )
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelMigrated
+        ( { accounts = old.accounts
+          , chunks = Dict.empty
+          }
+        , Cmd.none
+        )
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgOldValueIgnored
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgOldValueIgnored
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgOldValueIgnored
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgOldValueIgnored
---
+++ src/Evergreen/Type/V9.elm
@@ -0,0 +1,128 @@
+module Evergreen.Type.V9 exposing (..)
+
+import Character exposing (Character)
+import Dict exposing (Dict)
+import Hash exposing (Hash)
+import Lamdera exposing (ClientId)
+import Playground
+import World exposing (Chunk)
+
+
+devMode =
+    False
+
+
+type alias FrontendModel =
+    { page : Page
+    }
+
+
+type Page
+    = StartPage
+    | LoginPage LoginModel
+    | RegisterPage RegisterModel
+    | GamePage (Playground.Game Memory)
+
+
+type alias Memory =
+    { player : Character
+    , others : Dict String Character
+    , chunks : Dict ( Int, Int ) (Request Chunk)
+    , messages : List ( Int, Message )
+    , chatInput : Maybe String
+    , messageI : Int
+    }
+
+
+type alias Message =
+    { username : String, skin : Int, message : String }
+
+
+type Request a
+    = Pending
+    | Received a
+
+
+type alias BackendModel =
+    { accounts : List Account
+    , chunks : Dict ( Int, Int ) Chunk
+    }
+
+
+type alias Account =
+    { username : String
+    , passwordHash : Hash
+    , character : Character
+    , loggedIn : Maybe ClientId
+    }
+
+
+type FrontendMsg
+    = LoginMsg LoginMsg
+    | RegisterMsg RegisterMsg
+    | GameMsg Playground.Msg
+    | GotoLogin
+    | GotoRegister
+    | KeyDown String
+    | ChatInput String
+    | ChatSubmit
+    | RemoveMessage Int
+    | Noop
+
+
+type ToBackend
+    = CheckName String
+    | CreateAccount String Hash Int
+    | Login String Hash
+    | UpdatePlayer Character
+    | SendMessage String
+    | GetChunk Int Int
+
+
+type BackendMsg
+    = BNoop
+
+
+type ToFrontend
+    = LoggedIn Account (Dict String Character)
+    | OtherLoggedIn Int String
+    | CheckNameResponse Bool
+    | WrongUsernameOrPassword
+    | UsernameAlreadyExists
+    | UpdateOtherPlayer String Character
+    | GotMessage Message
+    | ChunkResponse Int Int Chunk
+
+
+type alias RegisterModel =
+    { username : String
+    , password : String
+    , password2 : String
+    , characterPicker : Bool
+    , character : Maybe Int
+    , failed : Bool
+    , blurred : Bool
+    }
+
+
+type RegisterMsg
+    = InputUsername String
+    | InputPassword String
+    | InputPassword2 String
+    | Blurred
+    | Next
+    | SelectedCharacter Int
+    | Register
+
+
+type alias LoginModel =
+    { username : String
+    , password : String
+    , failed : Bool
+    }
+
+
+type LoginMsg
+    = LoginUsername String
+    | LoginPassword String
+    | Submit
